85b6b64d845f04e7339ea0d3c75374701baebfc3
Larry <pascu.laurentiu17@gmail.com>
2019-04-11 15:20:23 +0300

    update script, reset style when click outside btn

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
+const body      = document.getElementsByTagName('body')[0];
 const searchBtn = document.getElementById('search-btn');
 const search    = document.getElementById('search');
 const tip       = document.getElementById('tip');
 
-searchBtn.addEventListener('click', () => {
-    search.style.width = '80%';
-    search.style.paddingLeft = '60px';
-    search.style.cursor = 'text';
-    search.focus();
+var i = 0;
+var message = 'Men Who Love Weirdos';
+var speed = 100;
 
-
-    var i = 0;
-    var message = 'Men Who Love Weirdos';
-    var speed = 100;
-
-    function typeWriter() {
-        if (i < message.length) {
-            msg = search.getAttribute('placeholder') + message.charAt(i);
-            search.setAttribute('placeholder', msg);
-            i++;
-            setTimeout(typeWriter, speed);
-        }
+body.addEventListener('click', (e) => {
+    if(searchBtn.contains(e.target)) {
+        search.style.width = '80%';
+        search.style.paddingLeft = '60px';
+        search.style.cursor = 'text';
+        search.focus();
+        typeWriter();
+    } else {
+        search.setAttribute('placeholder', '');
+        i = 0;
+        search.style.width = '';
+        search.style.paddingLeft = '';
+        search.style.cursor = 'pointer';
     }
-
-    typeWriter();
+    
 });
 
 search.addEventListener('keydown', () => {
     tip.style.visibility = 'visible';
     tip.style.opacity = '1';
 });
+
+function typeWriter() {
+    if (i < message.length) {
+        msg = search.getAttribute('placeholder') + message.charAt(i);
+        search.setAttribute('placeholder', msg);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
